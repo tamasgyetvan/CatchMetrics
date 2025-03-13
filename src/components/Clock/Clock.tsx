9bb9a3ea94d839f802111b579c47e38c8731412c
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2025-03-13 15:16:49 +0100

    Create Clock component

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import styles from "./Clock.module.css";
+type CurrentTime = {
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
+export function Clock() {
+  const [currentTime, setCurrentTime] = useState<CurrentTime>();
+
+  useEffect(() => {
+    setInterval(() => {
+      const time = new Date();
+
+      const hours = time.getHours();
+      const minutes = time.getMinutes();
+      const seconds = time.getSeconds();
+
+      setCurrentTime({ hours, minutes, seconds });
+    }, 1000);
+  });
+
+  return (
+    <div className={styles.clock}>
+      <img
+        src="src/assets/clock-two-svgrepo-com(1).svg"
+        alt="Clock Logo"
+        className={styles.icon}
+      />
+
+      <div>
+        <span className="hours">{currentTime?.hours}:</span>
+        <span className="minutes">{currentTime?.minutes}:</span>
+        <span className="seconds">{currentTime?.seconds}</span>
+      </div>
+      <h2 className={styles.title}>Current time</h2>
+    </div>
+  );
+}
