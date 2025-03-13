e29f7217837f0d61f46ed1d932c1c91426d2a128
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2025-03-13 21:16:20 +0100

    Replace svg

--- src/components/CountDownTimer/CountDownTimer.tsx
+++ src/components/CountDownTimer/CountDownTimer.tsx
@@ -3,10 +3,24 @@ import styles from "./CountDownTimer.module.css";
 type CountDownTimerProps = {
   remainingTime: number;
 };
+
+function isOneDigit(number: number) {
+  const minutes = number % 60;
+  if (minutes.toString().length > 1) {
+    return false;
+  }
+  return true;
+}
+
+console.log(isOneDigit(5));
+console.log(isOneDigit(15));
 export function CountDownTimer({ remainingTime }: CountDownTimerProps) {
   return (
     <div className={styles.countDownTimer}>
-      {Math.floor(remainingTime / 60)}:{remainingTime % 60}
+      {Math.floor(remainingTime / 60)}:
+      {isOneDigit(remainingTime)
+        ? "0" + (remainingTime % 60)
+        : remainingTime % 60}
     </div>
   );
 }
